04a7d3576e4556dbff6062d4e8cac121e9e83923
rafedraper <rafedraper@gmail.com>
2020-08-19 14:35:26 +0200

    accepting the first command

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
-const alanKey = "";
+// const alanKey = '4b3712bf053b32d0d882e0e0226df3102e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
   useEffect(() => {
     alanBtn({
-      key: alanKey,
+      key: '4b3712bf053b32d0d882e0e0226df3102e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles }) => {
+        if (command === 'newHeadlines') {
+          console.log(articles);
+        }
+      }
     });
   }, []);
   return (
