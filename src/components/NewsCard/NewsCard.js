715d9533012bb1cdc0dd96dee882d9c330694231
rafedraper <rafedraper@gmail.com>
2020-08-20 15:48:28 +0200

    working on the material UI

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React from "react";
+import {
+  Card,
+  CardActions,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
 
-const NewsCard = () => {
-    return ( 
+import useStyles from "./styles.js";
+
+const NewsCard = ({
+  article: { description, publishedAt, source, title, url, urlToImage },
+  i,
+}) => {
+  const classes = useStyles();
+  return (
     <div>
-        News Card
-    </div> 
-    );
-}
- 
+      <Card>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={
+              urlToImage ||
+              "https://www.pinnacleeducations.in/wp-content/uploads/2017/05/no-image.jpg"
+            }
+          />
+          <div>
+            <Typography variant="body2" color="textSecondary" component="h2">
+              {new Date(publishedAt).toDateString()}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="h2">
+              {source.name}
+            </Typography>
+          </div>
+          <Typography gutterBottom variant="h5">
+            {title}
+          </Typography>
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+          <Typography variant="h5" color="textSecondary">
+            {i + 1}
+          </Typography>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
+
 export default NewsCard;
--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import NewsCard from '../NewsCard/NewsCard'
+import React from "react";
+import { Grid, Grow, Typography } from "@material-ui/core";
 
-const NewsCards = ({articles}) => {
-    return ( 
-    <div>
+import useStyles from "./styles.js";
+import NewsCard from "../NewsCard/NewsCard";
+
+const NewsCards = ({ articles }) => {
+  const classes = useStyles();
+
+  return (
+    <Grow in>
+      <Grid
+        className={classes.container}
+        container
+        alignItems="stretch"
+        spacing={3}
+      >
         {articles.map((article, i) => (
-            <NewsCard />
+          <Grid item xs={12} sm={6} style={{ display: "flex" }}>
+            <NewsCard article={article} i={i} />
+          </Grid>
         ))}
-    </div> );
-}
- 
+      </Grid>
+    </Grow>
+  );
+};
+
 export default NewsCards;
